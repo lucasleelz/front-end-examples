bd0dd05f9141c85d2492133386e6a4bb91ea0443
lucas lee <llzqingdao2012@gmail.com>
2017-10-02 17:12:45 +0800

    数据存储类方法后面都加上分号。

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -4,23 +4,23 @@
 
   function DataStore() {
     this.data = {};
-  }
+  };
 
   DataStore.prototype.add = function(key, value) {
     this.data[key] = value;
-  }
+  };
 
   DataStore.prototype.get = function(key) {
     return this.data[key];
-  }
+  };
 
   DataStore.prototype.findAll = function(key) {
     return this.data;
-  }
+  };
 
   DataStore.prototype.remove = function(key) {
     delete this.data[key];
-  }
+  };
 
   App.DataStore = DataStore;
   window.App = App;

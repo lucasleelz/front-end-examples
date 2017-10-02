d682997c9107021f2a6ed0b15dded3edf3e09adf
lucas lee <llzqingdao2012@gmail.com>
2017-10-02 16:46:39 +0800

    添加数据存储类。

---
+++ coffeerun/scripts/datastore.js
@@ -0,0 +1,27 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+
+  function DataStore() {
+    this.data = {};
+  }
+
+  DataStore.prototype.add = function(key, value) {
+    this.data[key] = value;
+  }
+
+  DataStore.prototype.get = function(key) {
+    return this.data[key];
+  }
+
+  DataStore.prototype.findAll = function(key) {
+    return this.data;
+  }
+
+  DataStore.prototype.remove = function(key) {
+    delete this.data[key];
+  }
+
+  App.DataStore = DataStore;
+  window.App = App;
+})(window);

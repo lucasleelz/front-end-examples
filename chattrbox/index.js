1e6d247fbd0421d13dd9fa2c841c9fc6e248bc49
lucas lee <llzqingdao2012@gmail.com>
2017-10-03 14:45:37 +0800

    处理url。

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -3,6 +3,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
   console.log('Responding to a request');
+  var url = request.url;
+
+  var fileName = 'index.html';
+  if (url.length > 1) {
+    fileName = url.substring(1);
+  }
+  console.log(fileName);
   fs.readFile('app/index.html', function(error, data) {
       response.end(data);
   });

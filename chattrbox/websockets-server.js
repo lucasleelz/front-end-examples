9c4d92b71af7c269d24b9eb6b76f9022c6955ef3
lucas lee <llzqingdao2012@gmail.com>
2017-10-03 15:16:53 +0800

    聊天服务器的功能。

--- chattrbox/websockets-server.js
+++ chattrbox/websockets-server.js
@@ -7,10 +7,19 @@ var ws = new WebSocketServer({
 
 console.log('WebSockets server started');
 
+var messages = [];
+
 ws.on('connection', function(socket) {
   console.log('client connection established');
-  socket.on('message', function(data) {
-    console.log('message received: ' + data);
-    socket.send(data);
+  messages.forEach(function(message) {
+    socket.send(message);
+  });
+  socket.on('message', function(message) {
+    console.log('message received: ' + message);
+    messages.push(message);
+    // 对所有连接上的终端都发送最新的消息。
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(message);
+    })
   });
 });

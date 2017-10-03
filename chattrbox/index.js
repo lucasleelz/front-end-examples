d69846a785541e57d12963c0a4394387ddf8582c
lucas lee <llzqingdao2012@gmail.com>
2017-10-03 14:55:12 +0800

    处理错误。

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -2,12 +2,21 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 
+var handlerError = function(error, response) {
+  response.writeHead(404);
+  response.end();
+}
+
 var server = http.createServer(function(request, response) {
   console.log('Responding to a request');
   var url = request.url;
 
   var filePath = extract(request.url);
   fs.readFile(filePath, function(error, data) {
+    if (error) {
+      handlerError(error, response);
+      return;
+    }
     response.end(data);
   });
 });

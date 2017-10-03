70fc3216bf2a9389fe7b994d93f7bfb9ec40f6db
lucas lee <llzqingdao2012@gmail.com>
2017-10-03 14:35:42 +0800

    hello node.

---
+++ chattrbox/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  console.log('Responding to a request');
+  response.end('<h1>Hello World</h1>')
+});
+
+server.listen(3000);

00b06de8393ede5f56ccb2bb1bb4a8e605381f97
lucas lee <llzqingdao2012@gmail.com>
2017-10-03 14:43:34 +0800

    fs 读取文件。

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
   console.log('Responding to a request');
-  response.end('<h1>Hello World</h1>')
+  fs.readFile('app/index.html', function(error, data) {
+      response.end(data);
+  });
 });
 
 server.listen(3000);

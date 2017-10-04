805df4f315967169a09ccd944fa90a349432e269
lucas lee <llzqingdao2012@gmail.com>
2017-10-04 12:27:48 +0800

    使用ember generate生成适配器。

--- tracker/app/routes/witnesses.js
+++ tracker/app/routes/witnesses.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let witnessRecord = this.store.createRecord('witness', {
-      firstName: 'lucas',
-      lastName: 'lee',
-      email: 'llzqingdao2012@gmail.com'
-    });
-    return [witnessRecord];
+    return this.store.findAll('witness');
   }
 });

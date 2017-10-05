4824feedd80ea046899a60eddde436f916785af6
lucas lee <llzqingdao2012@gmail.com>
2017-10-05 11:22:46 +0800

    解决witness fullname显示错误的问题。

--- tracker/app/models/witness.js
+++ tracker/app/models/witness.js
@@ -3,11 +3,11 @@ import DS from 'ember-data';
 
 // 目击者
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
+  fName: DS.attr('string'),
+  lName: DS.attr('string'),
   email: DS.attr('string'),
   sightings: DS.hasMany('sighting'),
-  fullName: Ember.computed('firstName', 'lastName', function() { // 计算属性.
-    return this.get('firstName') + ' ' + this.get('lastName');
+  fullName: Ember.computed('fName', 'lName', function() { // 计算属性.
+    return this.get('fName') + ' ' + this.get('lName');
   })
 });
